96bbc60f6b1226c73655c2fe22a1cf48c5d8ba22
Matthew <68716884+2406mmartin@users.noreply.github.com>
2025-02-08 15:01:12 -0600

    Further development

--- ARD1/ARD1.ino
+++ ARD1/ARD1.ino
@@ -10,13 +10,15 @@
 #define start 2
 #define dirPin 8
 #define stepPin 9
-#define stepper_enable 12
+#define stepperEnable 12
 #define stepsPerRevolution 200
+#define microStep 11
 
 // Mode and Arrays
 String stepMode = "LEFT";
 bool addDebounce = false;
 bool nextDebounce = false;
+bool stepDebounce = false;
 int stepLeftArr[6] = {0, 0, 0, 0, 0, 0};
 int stepRightArr[6] = {0, 0, 0, 0, 0, 0};
 int currentSlot = 0;
@@ -27,15 +29,24 @@ String stepPrefix = "Step:";
 // Hardware Setup
 LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules
 
+int arrayToInt(int arr[], int size) {
+    int result = 0;
+    for (int i = 0; i < size; ++i) {
+        result = result * 10 + arr[i];
+    }
+    return result;
+}
+
 void setup() {
     pinMode(add, INPUT_PULLUP);
     pinMode(next, INPUT_PULLUP);
     pinMode(prime_right, INPUT_PULLUP);
     pinMode(prime_left, INPUT_PULLUP);
-    pinMode(start, INPUT_PULLUP);    
+    pinMode(start, INPUT_PULLUP);
     pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
-    pinMode(stepper_enable, OUTPUT);
+    pinMode(stepperEnable, OUTPUT);
+    pinMode(microStep, OUTPUT);
 
     Wire.begin();
     lcd.begin(&Wire);
@@ -46,50 +57,39 @@ void setup() {
 }
 
 void loop() {
-    //disable stepper
-    digitalWrite(stepper_enable, HIGH);
+    // Disable stepper
+    digitalWrite(stepperEnable, HIGH);
+
     // Add Button Functionality
-    if (digitalRead(add) == LOW && addDebounce == false) {
+    if (digitalRead(add) == LOW && !addDebounce) {
         addDebounce = true;
         if (stepMode == "LEFT") {
-            if (stepLeftArr[currentSlot] < 9) {
-              stepLeftArr[currentSlot] = stepLeftArr[currentSlot] + 1;
-            } else {
-              stepLeftArr[currentSlot] = 0;
-            }
+            stepLeftArr[currentSlot] = (stepLeftArr[currentSlot] < 9) ? stepLeftArr[currentSlot] + 1 : 0;
         } else if (stepMode == "RIGHT") {
-            if (stepRightArr[currentSlot] < 9) {
-              stepRightArr[currentSlot] = stepRightArr[currentSlot] + 1;
-            } else {
-              stepRightArr[currentSlot] = 0;
-            }
+            stepRightArr[currentSlot] = (stepRightArr[currentSlot] < 9) ? stepRightArr[currentSlot] + 1 : 0;
         }
-    } else if (digitalRead(add) == HIGH && addDebounce == true) {
+    } else if (digitalRead(add) == HIGH && addDebounce) {
         addDebounce = false;
     }
 
     // Next Button Functionality
-    if (digitalRead(next) == LOW && nextDebounce == false) {
+    if (digitalRead(next) == LOW && !nextDebounce) {
         nextDebounce = true;
         if (currentSlot < 5) {
-            currentSlot += 1;
+            currentSlot++;
         } else {
-            if (stepMode == "LEFT") {
-              stepMode = "RIGHT";
-            } else {
-              stepMode = "LEFT";
-            }
+            stepMode = (stepMode == "LEFT") ? "RIGHT" : "LEFT";
             currentSlot = 0;
         }
-    } else if (digitalRead(next) == HIGH && nextDebounce == true) {
+    } else if (digitalRead(next) == HIGH && nextDebounce) {
         nextDebounce = false;
     }
 
     lcd.clear();
 
     // Step Mode Handler & Print to LCD
+    String printable = stepPrefix;
     if (stepMode == "LEFT") {
-        String printable = stepPrefix;
         for (int i = 0; i < 6; i++) {
             printable += String(stepLeftArr[i]);
         }
@@ -99,7 +99,6 @@ void loop() {
         lcd.setCursor(15, 0);
         lcd.print("LEFT");
     } else if (stepMode == "RIGHT") {
-        String printable = stepPrefix;
         for (int i = 0; i < 6; i++) {
             printable += String(stepRightArr[i]);
         }
@@ -110,34 +109,43 @@ void loop() {
         lcd.print("RIGHT");
     }
 
-    delay(20);
-
     //-------------STEPPER PRIME-----------//
 
     // Set motor direction clockwise
     digitalWrite(dirPin, HIGH);
+    digitalWrite(microStep, LOW);
 
     // Spin motor
-    bool move_motor = false;
-    if (digitalRead(prime_left)==0){
-      move_motor=true;      
+    bool move_motor = (digitalRead(prime_left) == LOW);
+    
+    while (move_motor) {
+        digitalWrite(stepperEnable, LOW);
+        for (int x = 0; x < stepsPerRevolution; x++) {
+            digitalWrite(stepPin, HIGH);
+            delayMicroseconds(2000);
+            digitalWrite(stepPin, LOW);
+            delayMicroseconds(2000);
+        }
+        if (digitalRead(prime_left) == HIGH) {
+            move_motor = false;
+            digitalWrite(stepperEnable, HIGH);
+            return;
+        }
     }
-    else {
-      move_motor=false;
+
+    const int stp = 2000;
+    if (digitalRead(start) == LOW && stepDebounce == false) {
+        stepDebounce = true;
+        digitalWrite(stepperEnable, LOW);
+        digitalWrite(microStep, HIGH);
+        for (int x = 0; x < stp; x++) {
+            digitalWrite(stepPin, HIGH);
+            delayMicroseconds(2000);
+            digitalWrite(stepPin, LOW);
+            delayMicroseconds(2000);
+        }
+        while (true);
     }
-    while(move_motor){
-      digitalWrite(stepper_enable, LOW);
-      for(int x = 0; x < stepsPerRevolution; x++)
-          {
-              digitalWrite(stepPin, HIGH);
-              delayMicroseconds(2000);
-              digitalWrite(stepPin, LOW);
-              delayMicroseconds(2000);
-          }
-      if (digitalRead(prime_left==1)){
-        move_motor=false;
-        digitalWrite(stepper_enable, HIGH);
-        return 0;
-      }
-    }
+
+    delay(20);
 }
